dbb36f66fffa33804e50be9499ace4a1274b2572
hide4096 <87698678+hide4096@users.noreply.github.com>
2022-10-27 12:11:35 +0900

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,15 @@
+//SPDX-FileCopyrightText: 2022 Aso Hidetoshi asouhide2002@gmail.com
+//SPDX-License-Identifier: BSD-3-Clause
+
 #include <Arduino.h>
 #include <WiFi.h>
 #include <WiFiScan.h>
 
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin(115200);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  //scanNetworksの引数
   //非同期モード ステルスSSIDのチェック パッシブスキャン スキャン間隔(チャンネル毎) チャンネル SSID
   WiFi.scanNetworks(false,false,false,100,1,"Saponse");
   int val = WiFi.RSSI(0);
